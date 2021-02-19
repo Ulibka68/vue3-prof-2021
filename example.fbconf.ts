854346423e61716b470f1c3e5bdcd1c8ba13d7fd
Ulibka68 <gena68@inbox.ru>
2021-02-19 09:57:29 +0300

    first commit

--- example.fbconf.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIza",
-  authDomain: "vue3",
-  projectId: "vue3-practic",
-  storageBucket: "vue3-practic.appspot.com",
-  messagingSenderId: "55",
-  appId: "1:708:web:91",
-};
--- src/utils/firebase/addNewField.ts
+++
@@ -1,5 +0,0 @@
-import {fbAppDatabaseTs,loadFirebaseDatabaseAsyncModule} from './FBCustDatabase'
-export async function AddQuantityField() {
-  await loadFirebaseDatabaseAsyncModule();
-
-}
